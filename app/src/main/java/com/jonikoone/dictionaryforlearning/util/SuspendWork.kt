44339f56996a2f21e10352c92e43e3fd43ddc2e3
jonikoone <jonikoone@gmail.com>
2020-04-01 21:07:08 +0500

    add background differed with timeout  work

---
+++ app/src/main/java/com/jonikoone/dictionaryforlearning/util/SuspendWork.kt
@@ -0,0 +1,31 @@
+package com.jonikoone.dictionaryforlearning.util
+
+import kotlinx.coroutines.*
+import timber.log.Timber
+import kotlin.coroutines.CoroutineContext
+
+interface SuspendWork<T> : CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
+
+    var job: Job?
+    val delaySuspend: Long
+    val work: (T) -> Unit
+
+    fun createJob(t: T) = launch {
+        delay(delaySuspend)
+        work(t)
+    }
+
+    fun backgroundWork(t: T) {
+        job?.cancel()
+        job = createJob(t)
+    }
+}
+
+class SuspendWorkDefault<T>(override val delaySuspend: Long = 300) : SuspendWork<T> {
+    override var job: Job? = null
+    override val work: (T) -> Unit = {
+        Timber.d("Default implementation is not override!")
+    }
+}
